795ae210c989922b9d066bf203a4ee4a9b0aa27a
DatTien2107 <chytai2012@gmail.com>
2024-10-01 20:57:29 +0700

    new file:   ui/src/assets/imageLogin/image.png         modified:   ui/src/pages/login/index.jsx         new file:   ui/src/pages/login/login.scss

--- ui/src/pages/login/index.jsx
+++ ui/src/pages/login/index.jsx
@@ -1,7 +1,104 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./login.scss";
+import loginImage from "./image.png";
 
-export default function index() {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Đăng nhập với:", email, password);
+  };
+
   return (
-    <div>index</div>
-  )
+    <div className="login-container">
+      <img src={loginImage} alt="Login" className="login-image" />
+      <h2>Đăng nhập</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Tên đăng nhập</label>
+          <input
+            type="text"
+            id="email"
+            className="login-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Mật khẩu</label>
+          <input
+            type="password"
+            id="password"
+            className="login-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="remember-me">
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember">Ghi nhớ đăng nhập</label>
+        </div>
+        <button type="submit" className="login-button">
+          Đăng nhập
+        </button>
+      </form>
+      <Link to="/forget-password" className="forget-password-link">
+        Quên mật khẩu?
+      </Link>
+      <div className="divider"></div>
+      <p className="social-login-text">Hoặc đăng nhập bằng:</p>
+      <div className="social-login">
+        <button className="google-login">
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
+              <path
+                fill="#4285F4"
+                d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z"
+              />
+              <path
+                fill="#34A853"
+                d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z"
+              />
+              <path
+                fill="#FBBC05"
+                d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z"
+              />
+              <path
+                fill="#EA4335"
+                d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z"
+              />
+            </g>
+          </svg>
+        </button>
+        <button className="facebook-login">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#1877F2"
+              d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+            />
+          </svg>
+        </button>
+      </div>
+      <p className="signup-link">
+        Chưa có tài khoản? <Link to="/signup">Đăng ký ngay</Link>
+      </p>
+    </div>
+  );
 }
+
+export default Login;
