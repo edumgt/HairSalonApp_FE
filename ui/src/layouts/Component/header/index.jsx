05095718303982d401873c6b91b65890a43a245a
DatTien2107 <chytai2012@gmail.com>
2024-10-01 21:03:10 +0700

    new file:   ui/src/layouts/Component/footer/index.jsx         new file:   ui/src/layouts/Component/footer/index.scss         new file:   ui/src/layouts/Component/header/index.jsx         new file:   ui/src/layouts/Component/header/index.scss         new file:   ui/src/layouts/Component/layout/index.jsx

---
+++ ui/src/layouts/Component/header/index.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import "./index.scss";
+
+function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://30shine.com/static/media/logo_30shine_new.7135aeb8.png"
+          alt="logo"
+          className="header__logo"
+          width={100}
+          onClick={() => navigate("/home")} // Điều hướng về trang chủ
+          style={{ cursor: "pointer" }} // Thêm con trỏ để chỉ ra logo có thể click
+        />
+
+        <ul className="header__navigation">
+          <li>Trang chủ</li>
+          <li>Về 30shine</li>
+          <li>Học cắt tóc</li>
+          <li>Nhượng quyền</li>
+          <li>Đối tác</li>
+        </ul>
+      </div>
+      <div className="header__right">
+        <div
+          className="header__right__login-button"
+          onClick={() => navigate("/login")}
+        >
+          <span className="header__right__login-button__text">Đăng nhập</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ ui/src/layouts/Component/layout/index.jsx
@@ -0,0 +1,17 @@
+import Header from "../header";
+import { Outlet } from "react-router-dom";
+import Footer from "../footer";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+
+      <Outlet />
+
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
