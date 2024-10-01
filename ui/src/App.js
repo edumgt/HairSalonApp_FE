5beb7229525286b87897cec5ec8774e4d5772f16
DatTien2107 <chytai2012@gmail.com>
2024-10-01 20:51:26 +0700

    modified:   ui/src/App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Component/layout";
+import Home from "./page/home";
+import Login from "./page/login";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "/login", element: <Login /> },
+        { path: "/home", element: <Home /> },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
