07578da00fa04b5dd3fef57ac7fe998862179181
Khoa Nguyen <khoanqase184731@fpt.edu.vn>
2024-10-02 17:17:06 +0700

    update cho dung format

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -1,11 +1,4 @@
-const path = require('path');
-
 module.exports = function override(config, env) {
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    '@public': path.resolve(__dirname, 'public'),
-    '@ui': path.resolve(__dirname, 'src'),
-  };
-
+  // Thêm các cấu hình tùy chỉnh ở đây nếu cần
   return config;
-};
+}
